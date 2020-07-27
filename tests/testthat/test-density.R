8d9645ba75704420fe3d47657ae7f5897c882abf
joseah <alquicirajose@gmail.com>
2020-07-27 20:25:16 +1000

    styles code

--- tests/testthat/test-density.R
+++ tests/testthat/test-density.R
@@ -1,29 +1,26 @@
 test_that("calculate_density returns a vector of size n", {
+    em <- Seurat::Embeddings(Seurat::pbmc_small)[, 1:2]
+    w <- Seurat::GetAssayData(Seurat::pbmc_small)["CD8A", ]
+    res <- Nebulosa:::calculate_density(w, em, method = "ks")
 
-  em <- Seurat::Embeddings(Seurat::pbmc_small)[,1:2]
-  w <- Seurat::GetAssayData(Seurat::pbmc_small)["CD8A", ]
-  res <- Nebulosa:::calculate_density(w, em, method = "ks")
-
-  testthat::expect_vector(res, size = nrow(em))
+    testthat::expect_vector(res, size = nrow(em))
 })
 
 
 
 test_that("calculate-density returns a list with raw estimates", {
+    em <- Seurat::Embeddings(Seurat::pbmc_small)[, 1:2]
+    w <- Seurat::GetAssayData(Seurat::pbmc_small)["CD8A", ]
+    res <- Nebulosa:::calculate_density(w, em, method = "ks", map = FALSE)
 
-  em <- Seurat::Embeddings(Seurat::pbmc_small)[,1:2]
-  w <- Seurat::GetAssayData(Seurat::pbmc_small)["CD8A", ]
-  res <- Nebulosa:::calculate_density(w, em, method = "ks", map = FALSE)
-
-  testthat::expect_type(res, "list")
+    testthat::expect_type(res, "list")
 })
 
 
 test_that("calculate-density returns a list with raw estimates (sm)", {
+    em <- Seurat::Embeddings(Seurat::pbmc_small)[, 1:2]
+    w <- Seurat::GetAssayData(Seurat::pbmc_small)["CD8A", ]
+    res <- Nebulosa:::calculate_density(w, em, method = "sm", map = FALSE)
 
-  em <- Seurat::Embeddings(Seurat::pbmc_small)[,1:2]
-  w <- Seurat::GetAssayData(Seurat::pbmc_small)["CD8A", ]
-  res <- Nebulosa:::calculate_density(w, em, method = "sm", map = FALSE)
-
-  testthat::expect_type(res, "list")
+    testthat::expect_type(res, "list")
 })
