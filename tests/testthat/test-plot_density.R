5b4d401ca678d22da8547cb2852805bcdc15e180
joseah <alquicirajose@gmail.com>
2021-03-22 00:40:37 +1100

    fixes SeuratObject issue

--- tests/testthat/test-plot_density.R
+++ tests/testthat/test-plot_density.R
@@ -1,31 +1,31 @@
 test_that("plot_density returns a ggplot object", {
-    expect_is(plot_density(Seurat::pbmc_small, "CD8A", reduction = "pca"), "ggplot")
+    expect_is(plot_density(SeuratObject::pbmc_small, "CD8A", reduction = "pca"), "ggplot")
 })
 
 test_that("plot_density returns error if no feature is provided", {
-    expect_error(plot_density(Seurat::pbmc_small))
+    expect_error(plot_density(SeuratObject::pbmc_small))
 })
 
 test_that("plot_density returns error if feature is not present", {
-    expect_error(plot_density(Seurat::pbmc_small, "test"))
+    expect_error(plot_density(SeuratObject::pbmc_small, "test"))
 })
 
 test_that("plot_density returns error if non-existent reduction is provided", {
-    expect_error(plot_density(Seurat::pbmc_small, "CD8A", reduction = "test_function"))
+    expect_error(plot_density(SeuratObject::pbmc_small, "CD8A", reduction = "test_function"))
 })
 
 test_that("plot_density returns error if more than 2 dimensions are provided", {
-    expect_error(plot_density(Seurat::pbmc_small, dims = 1:3))
+    expect_error(plot_density(SeuratObject::pbmc_small, dims = 1:3))
 })
 
 test_that("plot_density returns error if only one dimension provided", {
-    expect_error(plot_density(Seurat::pbmc_small, dims = 1))
+    expect_error(plot_density(SeuratObject::pbmc_small, dims = 1))
 })
 
 test_that("plot_density returns error if data slot is not present", {
-    expect_error(plot_density(Seurat::pbmc_small, "CD8A", slot = "test_function"))
+    expect_error(plot_density(SeuratObject::pbmc_small, "CD8A", slot = "test_function"))
 })
 
 test_that("plot_density returns error if unknown method is provided", {
-    expect_error(plot_density(Seurat::pbmc_small, "CD8A", method = "test_method"))
+    expect_error(plot_density(SeuratObject::pbmc_small, "CD8A", method = "test_method"))
 })
